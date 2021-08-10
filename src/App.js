ae843122740130a02f12cc3604ea3a2b1ff8e3a2
Zsolt HORVATH <h.zsola@gmail.com>
2021-08-10 15:33:56 +0200

    Iteration 1 is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
+import contacts from "./contacts.json";
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  let contactList = contacts.slice(0,5);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ContactsWrapper">
+      <table className="contactTable">
+        <thead>
+          <tr>
+            <th colSpan="3">IronContacts</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td >Picture</td>
+            <td >Name</td>
+            <td >Popularity</td>
+          </tr>
+
+          {contactList.map((contact)=>{
+            return <tr key={contact.id}>
+            <td ><img src={contact.pictureUrl} alt='image'></img></td>
+            <td >{contact.name}</td>
+            <td >{contact.popularity}</td>
+          </tr>
+          })}
+
+        </tbody>
+
+
+      </table>
     </div>
   );
 }
